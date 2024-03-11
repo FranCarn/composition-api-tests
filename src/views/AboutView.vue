7df2450e25d171b12c9ee6c8a7a5c4074835877f
@FranCarn <carnevalefranco99@gmail.com>
2024-03-11 20:29:23 -0300

    ref vs reactive & lifecycle hooks

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,70 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ title }}</h1>
+    <button @click="changeTitle">CHANGE</button>
   </div>
 </template>
+
+<script>
+import {
+  onActivated,
+  onBeforeMount,
+  onBeforeUpdate,
+  onDeactivated,
+  onErrorCaptured,
+  onMounted,
+  onRenderTracked,
+  onUnmounted,
+  onUpdated,
+  ref,
+} from "vue";
+export default {
+  setup() {
+    console.log("setup");
+
+    const title = ref("About Page");
+
+    onBeforeMount(() => {
+      console.log("onBeforeMount");
+    });
+
+    onMounted(() => {
+      console.log("onMounted");
+    });
+
+    onBeforeUpdate(() => {
+      console.log("onBeforeUpdate");
+    });
+
+    onUpdated(() => {
+      console.log("onUpdated");
+    });
+
+    onUnmounted(() => {
+      console.log("onUnmounted");
+    });
+
+    onErrorCaptured(() => {
+      console.log("onErrorCaptured");
+    });
+
+    onRenderTracked(() => {
+      console.log("onRenderTracked");
+    });
+
+    onActivated(() => {
+      console.log("onActivated");
+    });
+
+    onDeactivated(() => {
+      console.log("onDeactivated");
+    });
+    return {
+      title,
+      changeTitle: () => {
+        title.value = "New Title";
+      },
+    };
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Reactive vs Ref</h1>
+    <div>
+      <h3>{{ ironman }}</h3>
+      <h3>Is Alive: {{ ironmanIsAlive }}</h3>
+    </div>
+    <div>
+      <h3>{{ hulk }}</h3>
+      <h3>Is Alive: {{ hulkIsAlive }}</h3>
+    </div>
+    <button @click="changeIronman">Change Ironman</button>
+    <button @click="changeHulk">Change Hulk</button>
+    <button @click="changeStatus">Change status</button>
   </div>
 </template>
 
 <script>
+import { reactive, ref } from "vue";
+
 export default {
   name: "HomeView",
+  setup() {
+    const ironman = ref({ name: "Tony", age: 50 });
+    const hulk = reactive({ name: "Bruce", age: 40 });
+    const ironmanIsAlive = reactive(false);
+    const hulkIsAlive = ref(true);
+
+    const changeStatus = () => {
+      hulkIsAlive.value = !hulkIsAlive;
+      // ironmanIsAlive = !ironmanIsAlive;
+    };
+
+    return {
+      ironman,
+      hulk,
+      ironmanIsAlive,
+      hulkIsAlive,
+      changeIronman: () => {
+        ironman.value.name = "Tony Stark";
+        ironman.value.age = 55;
+      },
+      changeHulk: () => {
+        hulk.name = "Bruce Banner";
+        hulk.age = 45;
+      },
+      changeStatus,
+    };
+  },
 };
 </script>
