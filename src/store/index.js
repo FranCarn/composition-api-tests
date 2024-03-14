0edfc5874c4451e486a6543003d8bab1500400da
@FranCarn <carnevalefranco99@gmail.com>
2024-03-14 14:59:54 -0300

    todo skeleton & data in store ready

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,47 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    todos: [
+      {
+        id: "1",
+        text: "Vue is a progressive JavaScript framework",
+        completed: true,
+      },
+      {
+        id: "2",
+        text: "Vue was created by Evan You",
+        completed: false,
+      },
+      {
+        id: "3",
+        text: "Vue has a virtual DOM",
+        completed: true,
+      },
+      {
+        id: "4",
+        text: "Vue is known for its simplicity and ease of integration",
+        completed: false,
+      },
+      {
+        id: "5",
+        text: "Vue don't provides reactive data binding and composable view components",
+        completed: false,
+      },
+    ],
   },
   getters: {
+    allTodos(state, getters, rootState) {
+      return [...state.todos];
+    },
+    completedTodos(state, getters, rootState) {
+      return state.todos.filter((t) => t.completed);
+    },
+    pendingTodos(state, getters, rootState) {
+      return state.todos.filter((t) => !t.completed);
+    },
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {},
+});
--- src/views/TodoVuex.vue
+++ src/views/TodoVuex.vue
@@ -1,9 +1,58 @@
 <template>
-  <h1>TODO</h1>
+  <h1>TO-DO</h1>
+  <h4>Tasks pending: {{ pending.length }}</h4>
+  <hr />
+  <button>All</button>
+  <button>Pending</button>
+  <button>Completed</button>
+  <div v-if="all.length">
+    <ul>
+      <li
+        v-for="{ text, id, completed } in all"
+        :key="id"
+        :class="{ completed: completed }"
+      >
+        {{ text }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+export default {
+  setup() {
+    const { getters, commit, dispatch } = useStore();
+
+    return {
+      pending: computed(() => getters["pendingTodos"]),
+      completed: computed(() => getters["completedTodos"]),
+      all: computed(() => getters["allTodos"]),
+    };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+div {
+  display: flex;
+  justify-content: center;
+  text-align: "center";
+}
+ul {
+  width: 300px;
+  text-align: left;
+}
+li {
+  cursor: pointer;
+}
+.active {
+  background: #2c3e50;
+  color: #ffffff;
+}
+.completed {
+  text-decoration: line-through;
+}
+</style>
